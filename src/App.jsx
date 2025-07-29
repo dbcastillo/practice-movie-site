783e6d64afbcc6673b935b8884f6edaa0be40a2a
Derick Castillo <derickcastillo@Mac.attlocal.net>
2025-07-29 15:13:53 -0500

    Add Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Card = ({ title }) => {
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+            </div>
+            )
+    }
+
 const App = () => {
     return (
-        <h2>Functional Arrow Component</h2>
+        <div className="card-container">
+            <Card title="Star Wars" />
+            <Card title="Avatar" />
+            <Card title="The Lion King" />
+            </div>
         )
 }
 
